fe069e26f94d0b49b230ad1f5838b77c16b473fb
z00323h <alexander.simpson@target.com>
2018-09-10 01:29:51 -0500

    Finished Project 1

--- Projects/src/project_one/circle_math.py
+++ Projects/src/project_one/circle_math.py
@@ -1,23 +1,35 @@
 # Project 1
 # Given User input for a circles radius - Find the circumference and area
+from math import pi
 
 
 class CircleMath(object):
     def __init__(self):
-        self.radius = float(input("Please enter the radius of the Circle: "))
+        self.radius = input("Please enter the radius of the Circle: ")
 
+    # gets the circumference of the circle given radius
     def equate_circumference(self):
         try:
-            diameter = 2 * self.radius
-            print(diameter)
+            radius = float(self.radius)
+            circumference = 2 * pi * radius
+
+            print(f'The circumference of a circle given the radius {radius} is {circumference}.')
 
         except ValueError:
             print('Error, incorrect value')
 
+    # gets the area of the circle given radius
     def equate_area(self):
-        pass
+        try:
+            radius = float(self.radius)
+            area = 2 * (radius * radius)
+
+            print(f'The circumference of a circle given the radius {radius} is {area}.')
+
+        except ValueError:
+            print('Error, incorrect value')
 
 
-domath = CircleMath()
-domath.equate_area()
-domath.equate_circumference()
+circle_math = CircleMath()
+circle_math.equate_circumference()
+circle_math.equate_area()
