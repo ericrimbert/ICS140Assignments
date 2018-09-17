b6f73547e4c0ec86ad4a613181d85c1375f874ba
z00323h <alexander.simpson@target.com>
2018-09-17 13:38:51 -0500

    finished chapter 2

--- HomeWork/src/chapter_two/2.9.1.py
+++ HomeWork/src/chapter_two/2.9.1.py
@@ -1,1 +1,19 @@
 # 2.9.1 - Coordinate geometry
+
+# imports the square root function from the math
+from math import sqrt
+
+# variables that sets values to points
+x1 = 1.0
+y1 = 2.0
+x2 = 1.0
+y2 = 5.0
+
+x3 = (x2 - x1) * (x2 - x1)
+y3 = (y2 - y1) * (y2 - y1)
+
+# gets the distance
+point_dist = sqrt(x3 + y3)
+
+# outputs the distance
+print(f"The distance is {point_dist}.")
--- HomeWork/src/chapter_two/2.9.2.py
+++ HomeWork/src/chapter_two/2.9.2.py
@@ -1,1 +1,14 @@
 # 2.9.2 - Tree height
+
+# imports tan from math module
+from math import tan
+
+# variables that set the angle elevation and the shadow length
+angle_elev = 3.8
+shadow_len = 17.5
+
+# tree height
+tree_height = tan(angle_elev) * shadow_len
+
+# prints the tree height
+print(f"Tree height is: {tree_height}.")
