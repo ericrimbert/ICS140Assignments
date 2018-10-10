9c08ecdd5af8a58237ab9bea3f4c093bb6b2aa72
z00323h <alexander.simpson@target.com>
2018-10-09 23:14:59 -0500

    Added core features to hangman and run now is calling hangman.

--- Projects/src/project_three/hangman.py
+++ Projects/src/project_three/hangman.py
@@ -3,8 +3,37 @@ import random
 
 class Hangman(object):
     def __init__(self):
-        pass
+        self.attempts = 10
+        self.word = None
+        self.output_dict = {}
 
     def get_word(self):
         words = ['yellow', 'bird', 'house']
         word = words[random.randint(0, len(words) - 1)]
+
+        self.word = word
+        return word
+
+    def generate_word_output(self, word):
+        count = len(word)
+        i = 1
+        while i <= count:
+            self.output_dict[i] = "_"
+            i += 1
+
+        return self.output_dict
+
+    # takes a guessed letter, checks if it is in word
+    def check_guessed_letter(self, letter):
+        # sets i to count index and count to count correct amount of letters
+        i = 1
+        count = 0
+        for char in self.word:
+            if letter == char:
+                self.output_dict[i] = char
+                count += 1
+            i += 1
+
+        # returns count
+        return count
+
--- Projects/src/project_three/run.py
+++ Projects/src/project_three/run.py
@@ -1,6 +1,13 @@
+from Projects.src.project_three.hangman import Hangman
+hangman = Hangman()
+
 
 def main():
-    pass
+    word = hangman.get_word()
+    hangman.generate_word_output(word)
+    letter = input()
+    print(hangman.check_guessed_letter(letter))
+    print(hangman.output_dict)
 
 
 main()
