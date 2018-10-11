e33274ca60cf68a497290c9c03a7bae21a0c1b37
z00323h <alexander.simpson@target.com>
2018-10-10 23:16:46 -0500

    layed out some more core functions of the application and have the base game complete. Just need to do more validation/error handling then features

--- Projects/src/project_three/hangman.py
+++ Projects/src/project_three/hangman.py
@@ -4,16 +4,21 @@ import random
 class Hangman(object):
     def __init__(self):
         self.attempts = 10
+        self.word_length = None
+        self.letter_counter = 0
         self.word = None
         self.output_dict = {}
 
+    # gets a word from a dictionary at random
     def get_word(self):
         words = ['yellow', 'bird', 'house']
         word = words[random.randint(0, len(words) - 1)]
 
         self.word = word
+        self.word_length = len(word)
         return word
 
+    # sets the dictionary in the constructor to contain values of the word
     def generate_word_output(self, word):
         count = len(word)
         i = 1
@@ -36,4 +41,13 @@ class Hangman(object):
 
         # returns count
         return count
+
+    # returns the dictionary in a formatted output
+    def formatted_output(self):
+        output_string = ""
+        for key, char in self.output_dict.items():
+            output_string += char + " "
+
+        return output_string.strip()
+
 
--- Projects/src/project_three/run.py
+++ Projects/src/project_three/run.py
@@ -5,9 +5,24 @@ hangman = Hangman()
 def main():
     word = hangman.get_word()
     hangman.generate_word_output(word)
-    letter = input()
-    print(hangman.check_guessed_letter(letter))
-    print(hangman.output_dict)
+
+    print(f"Your word has {hangman.word_length} letters, good luck! ---> {hangman.formatted_output()}")
+    while hangman.attempts >= 0:
+        if hangman.attempts == 0:
+            print("You have no more attempts left! You Lose!")
+            break
+
+        guessed_letter = input("Enter in a letter: ").lower()
+        round_count = hangman.check_guessed_letter(guessed_letter)
+        if round_count:
+            print(f"You got {round_count} letter(s) correct! ---> {hangman.formatted_output()}")
+            hangman.letter_counter += round_count
+            if hangman.letter_counter == hangman.word_length:
+                print("You Win!")
+                break
+        else:
+            print(f"You did not get any letters! ---> {hangman.formatted_output()}")
+            hangman.attempts -= 1
 
 
 main()
