129928e6dadd9f5a21002d732f8418c05fbb168d
z00323h <alexander.simpson@target.com>
2018-10-12 23:21:34 -0500

    Added hangman ui. Not very good but should work. Only prints right now.

---
+++ Projects/src/project_three/ui.py
@@ -0,0 +1,14 @@
+head = "  0   "
+neck = " \|/  "
+torsoe = "  |     "
+legs = "  /\\  "
+dead_boii = f"  _________\n"\
+            f"  |       |\n"\
+            f"{head}    |\n"\
+            f"{neck}    |\n"\
+            f"{torsoe}  |\n"\
+            f"{legs}    |\n"\
+            f"          |\n"\
+            f"  ________|"
+
+print(dead_boii)
