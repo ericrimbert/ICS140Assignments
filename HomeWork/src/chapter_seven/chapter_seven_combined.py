35057c20ff3a1bff329f68350c96d4215d80e828
z00323h <alexander.simpson@target.com>
2018-10-29 23:28:50 -0500

    half of chapter 7 done.

---
+++ HomeWork/src/chapter_seven/chapter_seven_combined.py
@@ -0,0 +1,18 @@
+# 7.1.1: Slice a rhyme.
+#
+start_index = 4
+end_index = 7
+rhyme_lyric = 'The cow jumped over the moon.'
+sub_lyric = rhyme_lyric[start_index:end_index]
+print(sub_lyric)
+# 7.2.1: Format temperature output.
+#
+air_temperature = 36.4158102
+print("%.1f" % air_temperature + "C")
+
+# 7.3.1: Find abbreviation.
+#
+# 7.3.2: Replace abbreviation.
+#
+# 7.4.1: Extract area code.
+#
