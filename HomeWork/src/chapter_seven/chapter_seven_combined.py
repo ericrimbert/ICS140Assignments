c8ed4e3d3d284fa495a5c0dc0f8bdbed6f6ac23a
z00323h <alexander.simpson@target.com>
2018-10-30 22:34:35 -0500

    Finished chapter 7 homework

--- HomeWork/src/chapter_seven/chapter_seven_combined.py
+++ HomeWork/src/chapter_seven/chapter_seven_combined.py
@@ -12,7 +12,21 @@ print("%.1f" % air_temperature + "C")
 
 # 7.3.1: Find abbreviation.
 #
+user_tweet = 'I was LOL during the whole movie!'
+
+if "LOL" in user_tweet:
+    print('LOL means laughing out loud.')
+else:
+    print('No abbreviation.')
 # 7.3.2: Replace abbreviation.
 #
+user_tweet = 'Gotta go. I will TTYL.'
+decoded_tweet = user_tweet.replace('TTYL', 'talk to you later')
+
+print(decoded_tweet)
 # 7.4.1: Extract area code.
 #
+phone_number = '977-555-3221'
+number_segments = phone_number.split("-")
+area_code = number_segments[0]
+print('Area code:', area_code)
