e6e4892398a3f14c6493d2cb997bb617a055f8e4
z00323h <alexander.simpson@target.com>
2018-10-31 19:49:04 -0500

    Added finished lab 5

--- HomeWork/src/chapter_seven/lab5.py
+++ HomeWork/src/chapter_seven/lab5.py
@@ -1,9 +1,7 @@
-# capitalize () is a function that accepts a string as an argument.
-# Convert the first character of each word to an upper-case letter
-# and the remaining letters to lower-case letters. Return the resulting string.
-# Use the string functions split and join.
+# lab 5
 
 
+# returns true or false if the string is the same backwards
 def palindrom(string):
     string = string.lower()
     if string == string[::-1]:
@@ -12,17 +10,37 @@ def palindrom(string):
         return False
 
 
-# did not care for the split function and join.
+# returns string that has the first letter upper cased
 def capitalize(string):
+    # lowers all letters
     string = string.lower()
-    string_first = string[0].upper()
-    string = string.replace(string_first.lower(), string_first)
-    print(string)
+    # gets the first word in string
+    first_word = string.split()[0]
+    # splits the rest of the words into a list
+    string = string.split()
+    string = string[1::]
+    # capitalizes first letter in first_word
+    capital = first_word[0].upper()
+    # splits first word by the first letter then joins the new string and capitalizes letter
+    first_word = first_word.split(first_word[0])
+    first_word = capital.join(first_word)
+    # inserts first_word into first index of list
+    string.insert(0, first_word)
+    # joins list together with spaces
+    return ' '.join(string)
 
 
+# main function that gets input for word and phrase, does the needful
 def main():
-    print(palindrom("civic"))
-    capitalize("horse")
+    string = input("Enter in a word: ")
+    if palindrom(string):
+        print("Your word is a palindrom!")
+    else:
+        print("Your word is not a palindrom.")
+
+    phrase = input("Enter in a phrase, or multiple words: ")
+    phrase = capitalize(phrase)
+    print(phrase)
 
 
 main()
